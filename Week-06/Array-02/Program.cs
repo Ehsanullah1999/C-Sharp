056a247a25b633f9c8249bb873debbd0a2c698d2
Ehsanullah1999 <127827711+Ehsanullah1999@users.noreply.github.com>
2023-04-27 13:30:02 +0430

    Add files via upload

---
+++ Week-06/Array-02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[4] { 4, 6, 8, 10, };
+            int j = 0;
+            /*for(int i = 0; i < 4; i++)
+            {
+                a[i] = a[i] + 2;
+            }*/
+            foreach (int i in a)
+            {
+                j = i + 2;
+            }
+            /*for (int i = 0; i < 4; i++)
+            {
+                Console.Write(a[i] + "\t");
+            }*/
+            foreach(int i in a)
+            {
+                Console.Write(i +"\t");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week-06/Array/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] Score;
+            //int[] Score1 = { 5, 6, 7, 8, 9 };
+            //int[] Score2 = new int[4] { 3, 4, 5, 6 };
+            //int[] Score3 = new int[6];
+            int[] Score = new int[3];
+            String[] Lessons = new string[3];
+            int Sum = 0;
+            double Avg;
+
+            for (int i = 0; i < Lessons.Length; i++)
+            {
+                Console.WriteLine("Enter Lesson's name: ");
+                Lessons[i] = Console.ReadLine();
+                Console.WriteLine("Enter Lesson's Score: ");
+                Score[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < Lessons.Length; i++)
+            {
+                Sum = Sum + Score[i];
+            }
+            Avg = (double)Sum / Lessons.Length;
+
+            Console.WriteLine("  Lessons  |  Score  |  Category |  State");
+            Console.WriteLine("-----------|---------|-----------|-----------");
+
+            for (int i = 0; i < Lessons.Length; i++)
+            {
+                Console.Write($"{Lessons[i]}      |   {Score[i]}    |");
+                switch (Lessons[i])
+                {
+                    case string t when t.StartsWith("p"):
+                        Console.Write("PROGRAMING |");
+                        break;
+                    case string t when t.StartsWith("m"):
+                        Console.Write("    MATH   |");
+                        break;
+                    case string t when t.StartsWith("i"):
+                        Console.Write("  GENRAL   |");
+                        break;
+                }
+                if (Score[i] < 45)
+                {
+                    Console.Write(" FALIED  \n");
+                }
+                else if ((Score[i] >= 45) && (Score[i] <= 55))
+                {
+                    Console.Write(" TRY AGAIN \n");
+                }
+                else
+                    Console.Write(" PASSED \n");
+            }
+
+            Console.WriteLine("---------------------|----------------------");
+            Console.WriteLine($"        Averge       |       {Avg}        ");
+
+            Console.ReadLine();
+        }
+    }
+}
