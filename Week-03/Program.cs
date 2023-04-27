045630d33db07ceada84fcf589a03e99a4828136
Ehsanullah1999 <127827711+Ehsanullah1999@users.noreply.github.com>
2023-04-27 13:24:03 +0430

    Add files via upload

---
+++ Week-03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int A;
+            int Age;
+            string Name, Message;
+            Name = Console.ReadLine();
+            Age = Convert.ToInt32(Console.ReadLine());
+            //Age = int.Parse(Console.ReadLine());
+            //A = (double)Age;
+
+            //A = (int)Age;
+            Message = "your name is " + Name + " and you are age " + Age + " years old";
+            Console.Write("Print text by type 1 :");
+            Console.WriteLine(Message);
+            Console.Write("Print text by type 2 :");
+            Console.WriteLine("your name is " + Name + " and you are age " + Age + " years old");
+            Console.Write("Print text by type 3 :");
+            Console.WriteLine("your name is {0} and you are age {1} years old", Name, Age);
+            Console.Write("Print text by type 4 :");
+            Console.WriteLine($"your name is {Name} and you are age {Age} years old");
+            Console.ReadLine();
+        }
+    }
+}
