656bb147fe7c7ef68f9e7bd2a210fa51278eb735
Ehsanullah1999 <127827711+Ehsanullah1999@users.noreply.github.com>
2023-04-27 13:45:08 +0430

    Add files via upload

---
+++ Week-08/Funcation_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcation_1
+{
+    class Program
+    {
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        static int Sum1(int a, int b) => a + b;
+
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum of the two numbers is: " + Sum(x, y));
+            Console.WriteLine("Sum of the two numbers is: " + Sum1(x, y));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week-08/HomeWork/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static int Nag(int a, int b)
+        {
+            return a - b;
+        }
+        static int Nag1(int a, int b) => a - b;
+        static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        static int Mul1(int a, int b) => a * b;
+        static double Div(double a, double b)
+        {
+            return a / b;
+        }
+        static double Div1(double a, double b) => a / b;
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.Write("Enter first number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Nag of " + x + " and " + y + " is: " + Nag(x, y));
+            Console.WriteLine("Nag of " + x + " and " + y + " is: " + Nag1(x, y));
+
+            Console.WriteLine("Mul of " + x + " and " + y + " is: " + Mul(x, y));
+            Console.WriteLine("Mul of " + x + " and " + y + " is: " + Mul1(x, y));
+
+            Console.WriteLine("Div of " + x + " and " + y + " is: " + Div(x, y));
+            Console.WriteLine("Div of " + x + " and " + y + " is: " + Div1(x, y));
+            Console.ReadLine();
+        }
+    }
+}
