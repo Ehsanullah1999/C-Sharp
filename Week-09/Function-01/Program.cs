8d013633296c7663f902bcb78d94d5c9878f72a1
Ehsanullah Memar <127827711+Ehsanullah1999@users.noreply.github.com>
2023-05-07 10:47:31 +0430

    Add files via upload

---
+++ Week-09/Function-01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function_01
+{
+    class Program
+    {
+        static void Volume (int h, int l, int w)
+        {
+            Console.WriteLine("Volume is = " + h * l * w);
+        }
+
+        static int Area (int h, int l, int w)
+        {
+            int area = ((l * h) * 2) + ((l * w) * 2) + ((h * w) * 2); 
+            return area;
+        }
+
+        static int Peremeter (int h, int l, int w)
+        {
+
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            int hight, length, Width;
+            Console.WriteLine("***Enter the dimantions****");
+            Console.Write("Hight = ");
+            hight = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Length = ");
+            length = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Wifth = ");
+            Width = Convert.ToInt32(Console.ReadLine());
+
+            Volume(hight, length, Width);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week-09/Matrix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = { { 0, 0, 0, 0, 0, 0 },{ 1, 0, 0, 0, 0, 0}, { 0, 1, 0, 0, 0, 0}, { 1, 0, 1, 0, 0, 0 }, { 0, 0, 0, 1, 0, 0}, { 0, 0, 1, 0, 1, 0} };
+            int[,] Smatrix = new int[6, 6];
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (matrix[i, j] == 1)
+                    {
+                        Console.Write("Enter the space: ");
+                        Smatrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Smatrix[i, j] = 0;
+                    }
+                }
+            }
+                Console.WriteLine();
+                for (int i = 0; i < 6; i++)
+                {
+                    for (int j  = 0; j <= i; j++)
+                    {
+                        Console.Write(Smatrix[i, j] + "   ");
+                    }
+                Console.WriteLine();
+                }
+            Console.ReadLine();
+        }
+    }
+}
